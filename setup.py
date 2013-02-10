7b42383b7a627aea338708ddc376596c44fbea34
Will Meierjurgen Farr <wmfarr@gmail.com>
2013-02-09 18:34:51 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='plotutils',
+      version='0.0.1',
+      description='plotting utilities for probability density functions using matplotlib',
+      author='Will M. Farr',
+      author_email='w-farr@northwestern.edu',
+      url='http://github.com/farr/plotutils',
+      packages=['plotutils'])
+
